e4355ba497ed3df5f7bb0b8019f899e32d721d27
Andres Morey <andresmarcel@gmail.com>
2015-09-23 05:00:21 +0000

    implemented data-attributes, removed MUIRoundButton

--- src/react/buttons.jsx
+++ src/react/buttons.jsx
@@ -8,11 +8,7 @@
 var util = require('../js/lib/util.js'),
     Ripple = require('./ripple.jsx');
 
-var buttonClass = 'mui-btn',
-    flatClass = buttonClass + '-flat',
-    raisedClass = buttonClass + '-raised',
-    largeClass = buttonClass + '-lg',
-    floatingClass = buttonClass + '-floating';
+var buttonClass = 'mui-btn';
 
 
 /**
@@ -23,23 +19,19 @@ var Button = React.createClass({
   mixins: [Ripple],
   getDefaultProps: function() {
     return {
-      type: 'default',  // one of default, primary, danger or accent
+      color: 'default', // default|primary|danger|accent
+      style: 'default', // default|flat|raised|fab
+      size: 'default', // default|small|large
       disabled: false
     };
   },
   render: function() {
-    var cs = {};
-
-    cs[buttonClass] = true;
-    cs[buttonClass + '-' + this.props.type] = true;
-    cs[flatClass] = this.props.flat;
-    cs[raisedClass] = this.props.raised;
-    cs[largeClass] = this.props.large;
-    cs = util.classNames(cs);
-
     return (
       <button
-        className={ cs }
+        className={ buttonClass }
+        data-mui-color={ this.props.color }
+        data-mui-style={ this.props.style }
+        data-mui-size={ this.props.size }
         disabled={ this.props.disabled }
         onMouseDown={ this.ripple }
         onTouchStart={ this.ripple }
@@ -53,43 +45,7 @@ var Button = React.createClass({
 });
 
 
-/**
- * Round button constructor
- * @class
- */
-var RoundButton = React.createClass({
-  mixins: [Ripple],
-  getDefaultProps: function() {
-    return {
-      floating: true
-    };
-  },
-  render: function() {
-    var cs = {};
-
-    cs[buttonClass] = true;
-    cs[floatingClass] = true;
-    cs[floatingClass + '-mini'] = this.props.mini;
-    cs = util.classNames(cs);
-
-    return (
-      <button
-        className={ cs }
-        disabled={ this.props.disabled }
-        onMouseDown={ this.ripple }
-        onTouchStart={ this.ripple }
-        onClick={ this.props.onClick }
-      >
-        { this.props.children }
-        { this.state.ripples && this.renderRipples() }
-      </button>
-    );
-  }
-})
-
-
 /** Define module API */
 module.exports = {
-  Button: Button,
-  RoundButton: RoundButton
+  Button: Button
 };
